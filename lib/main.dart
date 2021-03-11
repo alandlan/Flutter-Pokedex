0e235b5c5f1bb5e8ee7692e725943eac9acf5276
Alan Vieira Martins <alanhorcrux@gmail.com>
2021-03-11 20:00:57 -0300

    adicionado dependencias

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(),
+      home: Scaffold(
+        body: Text('Ok'),
+      ),
     );
   }
 }
